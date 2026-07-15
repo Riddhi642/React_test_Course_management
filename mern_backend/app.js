1073966a00094fd23aff59367388ee035953ef48
Riddhi642 <riddhikhtate@gmail.com>
2026-07-15 19:13:04 +0530

    Fixed backend root route

--- mern_backend/app.js
+++ mern_backend/app.js
@@ -1,16 +1,31 @@
 const express = require('express');
-const {coonectDB} = require('./db');
+const express = require('express');
+const { connectDB } = require('./db');
 const routes = require('./router/courseRoute');
 const cors = require('cors');
-coonectDB()
-const app=express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use('/course',routes)
 
+const app = express();
+
+// Database Connection
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Home Route (Cannot GET / fix)
+app.get("/", (req, res) => {
+    res.send("Course Management Backend is running");
+});
+
+// Course Routes
+app.use('/course', routes);
+
+
+// Port
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
